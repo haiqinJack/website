a98ceadf8e759f19e53f658b06d80e29054da3f5
380105611@qq.com <380105611@qq.com>
2017-04-22 01:20:54 +0800

    添加导航，巨幕

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var multer = require('multer');
+var session = require('express-session');
+var flash = require('connect-flash');
+var ejsMate = require('ejs-mate');
+
+var routes = require('./routes');
+
+var app = express();
+
+//设置模板目录
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', ejsMate);
+
+//设置文件静态目录
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+//解析中间件 req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+//flash 中间价，用来显示通知
+app.use(flash());
+
+routes(app);
+
+app.listen(3000, function() {
+	console.log(`listen app success`);
+});
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function (app) {
+	app.get('/', function index(req, res) {
+		res.render('index/index', {
+
+		});
+	});
+};
