48da45a563b39fa10b976872e49ea17b06de2073
MAC <mac@MACtekiMacBook-Air.local>
2017-08-10 20:25:56 +0800

    v1.0

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,3 @@
-var express = require('express');
-var router = express.Router();
-var config = require('../configs/default');
-
 module.exports = function (app) {
 	app.get('/', function (req, res) {
 		res.render('index');
